ee487b80fb4c51fe52f2eb6a7581ead40adc1ded
Eric Liao <rcliao01@gmail.com>
2014-01-20 19:14:10 -0800

    gray scale and save file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 
 from Tkinter import Tk, Frame, Menu, Label, BOTH
-from tkFileDialog import askopenfilename
+from tkFileDialog import askopenfilename, asksaveasfilename
 
 from PIL import Image, ImageTk
 
@@ -30,11 +30,12 @@ class MenuFrame(Frame):
         
         fileMenu = Menu(menubar)
         fileMenu.add_command(label="Load", command=self.onLoad)
+        fileMenu.add_command(label="Save", command=self.onSave)
         fileMenu.add_command(label="Exit", command=self.onExit)
         menubar.add_cascade(label="File", menu=fileMenu)
 
         hw1Menu = Menu(menubar)
-        hw1Menu.add_command(label="Gray Scale")
+        hw1Menu.add_command(label="Gray Scale", command=self.onGray)
         hw1Menu.add_command(label="Resize Image")
         hw1Menu.add_command(label="Connect Component")
         hw1Menu.add_command(label="Erode/Dilate/Open/Close")
@@ -48,13 +49,14 @@ class MenuFrame(Frame):
     def onExit(self):
         self.quit()
 
+    """ Load the Image and show it on the GUI """
     def onLoad(self):
-        
         filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 
-        self.img = cv2.imread(filename, 1)
+        self.cv_img = cv2.imread(filename, 1)
+        self.img = cv2.cvtColor(self.cv_img, cv2.COLOR_RGB2BGR)
 
-        self.gray_img = Image.fromarray(cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR))
+        self.gray_img = Image.fromarray(self.img)
         self.gray_img_tk = ImageTk.PhotoImage(image=self.gray_img)
         
         self.label = Label(self.parent, image=self.gray_img_tk)
@@ -62,6 +64,21 @@ class MenuFrame(Frame):
         self.label.place(x=0, y=0)
         self.parent.update()
 
+    """ Save the Image into harddisk """
+    def onSave(self):
+        filename = asksaveasfilename() # show an "Save" dialog box and return the path to the selected file
+
+        cv2.imwrite(filename, self.img)
+
+    def onGray(self):
+        self.img = cv2.cvtColor(self.cv_img, cv2.COLOR_RGB2GRAY)
+        self.gray_img = Image.fromarray(self.img)
+        self.gray_img_tk = ImageTk.PhotoImage(image=self.gray_img)
+        
+        self.label = Label(self.parent, image=self.gray_img_tk)
+        self.label.pack()
+        self.label.place(x=0, y=0)
+        self.parent.update()
 
 
 def main():
